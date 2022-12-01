9ae20dd27ced562a775607984ed4a30cddd398fe
Aditya Khandare <khandareaditya97@gmail.com>
2022-11-30 18:57:32 -0600

    return json

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import bcrypt
 from basicauth import encode
 import json
-from flask import Flask, request
+from flask import Flask, request, redirect
 import ftplib
 import dotenv
 from dotenv import load_dotenv
@@ -12,6 +12,8 @@ app = Flask(__name__)
 
 
 resource_file = "ftpServer/passwords.json"
+uploadedFiles = []
+
 
 def encode(username,password):
     return (bcrypt.hashpw((username+password).encode('utf-8'), bcrypt.gensalt())).decode('utf-8')
@@ -46,9 +48,9 @@ def create_username():
     data = request.get_json(force=True)
     username,password = data["username"] , data["password"]
     if add_user(username,password):
-         return username
+        return {"result":"Successful"}
     else:
-        return "Failed"
+        return {"result":"Failed"}
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -57,13 +59,35 @@ def login():
     username,password = data["username"] , data["password"]
     if check_login(username,password):
         connectFTP()
-        return username
+        return {"result":"Successful"}
     else:
-       return "Failed"
+        return {"result":"Failed"}
 
 def connectFTP():
+    global ftp_server
     ftp_server = ftplib.FTP(os.getenv("HOSTNAME"), os.getenv("USERNAME"),os.getenv("PASSWORD") )
     ftp_server.encoding = "utf-8"
+
+
+# @app.route('/uploadFile', methods=['POST'])
+# def upload_file():
+#     uploaded_file = request.files['file']
+#     print(uploaded_file)
+#     print(uploaded_file.filename)
+#     if not uploaded_file.filename == "":
+#         uploaded_file.save(uploaded_file.filename)
+#         uploadedFiles.append(uploaded_file.filename)
+#         with open(uploaded_file.filename, "rb") as file:
+# # Command for Uploading the file "STOR filename"
+#             ftp_server.storbinary(f"STOR {uploaded_file.filename}", file)
+#         return "Uploaded"
+#     return "Failed"
+
+
+@app.route('/uploadFile', methods=['GET'])
+def fileList():
+    return uploadedFiles
+
 
 
 if __name__ == '__main__':
